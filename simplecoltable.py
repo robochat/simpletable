452c2d5a490404fce74b2f32adfa70031c6bdb2d
robochat <none@none>
2016-05-06 21:05:22 +0200

    ColTable: rewrote initialisation code in order to accept many kinds of data (untested). Code is quite involved, could it be simplified?

--- simplecoltable.py
+++ simplecoltable.py
@@ -16,18 +16,26 @@ from collections import Sequence,Mapping,Iterable,Hashable
 #extra methods: slicing by row, insertions, 
 #returning OrderedDicts for rows?
 #accept a sequence of column sequences?? how to do this without being too magical?
+#lose idea of columns having an order?
+#
 
 class ColTable(object):
-    """A very simple table class that supports index and column indexing.
+    """A simple table class that supports index and column indexing.
     Stores data by columns."""    
-    def __init__(self, items=None, headers=None, title=None, **kwargs):
+    def __init__(self, items=None, rowitems=None, headers=None, title=None, **kwargs):
         """Expects either a dictionary of columnar data, a sequence of 
-        row orientated data or a sequence of key,value paths. Even accepts
-        keyword arguements (excluding columns named title or headers!)
-        Checks that all entries have the same length. 
+        column orientated data or a sequence of (column-name,column-data)
+        tuples. Also accepts keyword arguements (excluding columns named
+        'items', 'rowitems', 'title' or 'headers'!). Checks that all entries
+        have the same length.
+        
+        Using the rowitems parameter, allows the class to accept row orientated
+        data such as a sequence of sequences or mappable items.        
         
         Input Parameters:
-            items -   iterable or mappable object.
+            items -   iterable or mappable object. Valid input for a dict or a
+                      sequence of columnar data.
+            rowitems- iterable row orientated data. (can't use both items and rowitems)
             headers - sequence of column names. If provided, this will determine
                       the column ordering.
             title -   sets a name for the dataset.
@@ -40,47 +48,45 @@ class ColTable(object):
         if headers and len(set(headers)) != len(headers): 
             raise ValueError("Class doesn't handle columns with duplicate names")
         
-        #handle sequence of rows
-        if (items and isinstance(items,Iterable)
-            #special exception: if the sequence entries are pairs of (Hashable,Iterable) then probably a 'serialised' dict
-            and not (isinstance(items[0],Iterable) and len(items[0]) ==2 
-                    and isinstance(items[0][0],Hashable) 
-                    and isinstance(items[0][1],Iterable)) 
-          ): 
-            height = len(items)
-            widths = (len(row) for row in items)
+        if items and rowitems: 
+            raise AssertionError("only one of items and rowitems parameters should be used")
+        
+        elif rowitems: #handle sequence of rows
+            height = len(rowitems)
+            widths = (len(row) for row in rowitems)
             width = widths.next()
             if not all(w == width for w in widths): raise ValueError('not all rows have the same number of columns')
             
             if headers:
                 datadict = OrderedDict((h,None) for h in headers) #preallocate (not for speed but to get order correct)
                 tmpheaders = list(headers)
-                for k in kwargs: del tmpheaders[tmpheaders.index(k)] #remove columns that are handled by keyword arguements
-                if len(tmpheaders) != width: raise ValueError('headers length does not match length of supplied data')
-                if isinstance(items[0],Iterable):
-                    for i,h in enumerate(tmpheaders):
-                        datadict[h] = [row[i] for row in items]
-                elif isinstance(Mapping):
-                    for h in tmpheaders:
-                        datadict[h] = [row[h] for row in items]
-                else:
-                    raise TypeError("'%s' object is not handled" %type(items))
                 
                 #handle the kwargs
                 for k,v in kwargs.iteritems():
                     if len(v) != height: raise ValueError('keyword entry (%s) does not match length of dataset' %k)
                     datadict[k] = v
+                    del tmpheaders[tmpheaders.index(k)] #remove columns that are handled by keyword arguements
                 
+                if len(tmpheaders) != width: raise ValueError('headers length does not match length of supplied data')
+                if isinstance(rowitems[0],Iterable):
+                    for i,h in enumerate(tmpheaders):
+                        datadict[h] = [row[i] for row in rowitems]
+                elif isinstance(Mapping):
+                    for h in tmpheaders:
+                        datadict[h] = [row[h] for row in rowitems]
+                else:
+                    raise TypeError("'%s' object is not handled" %type(rowitems))
+
             else:
-                if isinstance(items[0],Iterable): 
+                if isinstance(rowitems[0],Iterable): 
                     raise ValueError('headers sequence needs to be provided for this type of data')
-                elif isinstance(items[0],Mapping): #could be dict but could be OrderdDict or similar
-                    if len(set(row.keys() for row in items)) != width:  #all entries have the same keys?
+                elif isinstance(rowitems[0],Mapping): #could be dict but could be OrderdDict or similar
+                    if len(set(row.keys() for row in rowitems)) != width:  #all entries have the same keys?
                         raise ValueError('not all rows have the same number of columns')
-                    tmpheaders = items[0].keys()
+                    tmpheaders = rowitems[0].keys()
                     datadict = OrderedDict()
                     for h in tmpheaders:
-                        datadict[h] = [row[h] for row in items]
+                        datadict[h] = [row[h] for row in rowitems]
                     
                     #handle the kwargs
                     for k,v in kwargs.iteritems():
@@ -88,8 +94,36 @@ class ColTable(object):
                         datadict[k] = v
                 else:
                     raise TypeError("'%s' object is not handled" %type(items))
-
-        else:
+        
+        #columnar data -  iterable of sequences plus supplied header 
+        elif (items and isinstance(items,Iterable) 
+                and all(isinstance(item,Iterable for item in items))
+                #special exception: if the sequence entries are pairs of (Hashable,Iterable) then probably a 'serialised' dict
+                and not (len(items[0]) ==2 
+                        and isinstance(items[0][0],Hashable) 
+                        and isinstance(items[0][1],Iterable))
+               ):
+            if not headers: raise ValueError('headers sequence needs to be provided for this type of data')
+            width = len(items)
+            heights = (len(col) for col in items)
+            height = heights.next()
+            if not all(h == height for h in heights): raise ValueError('not all columns have the same number of rows')
+            
+            datadict = OrderedDict((h,None) for h in headers) #preallocate (not for speed but to get order correct)
+            tmpheaders = list(headers)
+            
+            #handle the kwargs
+            for k,v in kwargs.iteritems():
+                if len(v) != height: raise ValueError('keyword entry (%s) does not match length of dataset' %k)
+                datadict[k] = v
+                del tmpheaders[tmpheaders.index(k)] #remove columns that are handled by keyword arguements
+            
+            if len(tmpheaders) != width: raise ValueError('headers length does not match length of supplied data')
+            
+            for i,h in enumerate(tmpheaders):
+                datadict[h] = items[i]
+                
+        else: #columnar data - either via items or keywords
             datadict = OrderedDict(items,**kwargs)            
             # Reorder by headers sequence if provided
             if headers:
